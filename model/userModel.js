8bef3b7216c3b3f387587f829dba460c2c1290d4
vibhuti-jadav <vibhutij109@gmail.com>
2025-12-02 16:17:05 +0530

    add-login

--- model/userModel.js
+++ model/userModel.js
@@ -25,6 +25,15 @@ const userSchema =new mongoose.Schema({
 
 })
 
+
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+//login
 userSchema.statics.findByCredentials = async function (email, password) {
     const user = await this.findOne({ email });
     if (!user) {
